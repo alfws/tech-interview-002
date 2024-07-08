f7b4ba3423a14668bc962c9afb298d72b916b507
Kellouch <younes.kellouch@gmail.com>
2024-07-08 04:00:35 +0100

    Import the Compnents to out home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
+"use client";
+
+import ProductCard from "./components/ProductCard";
+import { Product } from "./types";
+import NavBar from "./components/NavBar";
+import { useState } from "react";
+
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([])
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Tech Interview x MapoTapo
-        </p>
+    <div >
+      <NavBar setProducts={setProducts} />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
-      <div className="flex flex-col mt-12 items-center justify-center w-full max-w-5xl">
-        <h1 className="text-4xl font-bold text-center">Welcome to Tech Interview</h1>
-        <div className="mt-8 text-center">
-          <p className="text-sm text-gray-600">
-            Read the README.md and solve the challenges!
-          </p>
-          </div>
-        </div>
-    </main>
-  )
+    </div>
+  );
 }
